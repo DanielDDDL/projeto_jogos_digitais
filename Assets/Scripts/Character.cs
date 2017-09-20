da661743331bbd73913440ffb169f7981df03d32
DanielDDDL <daniel.dddl.pdf@gmail.com>
2017-09-20 08:59:06 -0300

    assets iniciais e ideias sobre a movimentacao do personagem

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour {
+
+	float movSpeed = 2.0f, rotationSpeed = 2.5f;
+	float rotation = 5.0f;
+
+	void Start () {
+		
+	}
+
+	void Update () {
+
+		float xAxis = Input.GetAxis ("Horizontal");
+		float yAxis = Input.GetAxis ("Vertical");
+
+		rotation += (xAxis * rotationSpeed);
+		transform.eulerAngles = new Vector3(0, 0, rotation);
+
+		var move = new Vector3 (0, yAxis, 0);
+		transform.position += move * movSpeed * Time.deltaTime;
+
+	}
+}
